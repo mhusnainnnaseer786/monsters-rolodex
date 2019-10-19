5ffff8fb9a0c87bbda63bfe9a6f6173dfb3ef233
Muhammad Husnain Naseer <husnain.naseed@blueeast.com>
2019-10-19 10:13:02 +0500

    Updates

---
+++ precache-manifest.e13fc917e5dc28e0a8e3f838e77211c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c3c32030755d7df51cc883438cec6ef",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6c570b56a29d8a317409",
+    "url": "/monsters-rolodex/static/css/main.f749fbf4.chunk.css"
+  },
+  {
+    "revision": "6a7e489f5208e4660d0b",
+    "url": "/monsters-rolodex/static/js/2.b16f9c46.chunk.js"
+  },
+  {
+    "revision": "6c570b56a29d8a317409",
+    "url": "/monsters-rolodex/static/js/main.9d07cdd9.chunk.js"
+  },
+  {
+    "revision": "08bce0423d5802360779",
+    "url": "/monsters-rolodex/static/js/runtime~main.eb9c0a20.js"
+  }
+]);
